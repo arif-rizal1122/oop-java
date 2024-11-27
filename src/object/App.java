5f687aeead9e43d161a2c297d27590d286f2207a
arif-rizal1122 <rizallarif32@gmail.com>
2024-11-27 08:01:34 +0800

    object

---
+++ src/object/App.java
@@ -0,0 +1,11 @@
+package object;
+
+public class App {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        Person person2 = new Person();
+
+        System.out.println(person1);
+        System.out.println(person2);
+    }
+}
