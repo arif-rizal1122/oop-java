5f4caa94b4c612634545b8da90c919967e11d4be
arif-rizal1122 <rizallarif32@gmail.com>
2024-12-03 08:54:10 +0800

    anotation

---
+++ src/anotation/app/App.java
@@ -0,0 +1,13 @@
+package anotation.app;
+
+
+import anotation.anotation.Anot;
+
+// anot untuk menambhakan informasi saja... ini digunakan untuk metadata
+@Anot(name = "AnimalApp", tags = {"application java"})
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
